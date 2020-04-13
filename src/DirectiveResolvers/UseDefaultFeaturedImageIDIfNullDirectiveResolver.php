cd065c0f0479a69f712f9c654c891d741f502318
Leo <losoviz@gmail.com>
2020-04-13 19:18:22 +0800

    Added declare(strict_types=1);

--- src/DirectiveResolvers/UseDefaultFeaturedImageIDIfNullDirectiveResolver.php
+++ src/DirectiveResolvers/UseDefaultFeaturedImageIDIfNullDirectiveResolver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\PostMedia\DirectiveResolvers;
 
 use PoP\PostMedia\Environment;
--- src/FieldResolvers/PostFieldResolver.php
+++ src/FieldResolvers/PostFieldResolver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\PostMedia\FieldResolvers;
 
 use PoP\Media\Misc\MediaHelpers;
